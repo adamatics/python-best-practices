9791208b12d06e3adc367284c9ffc558faf5934a
salvador-g-c <sagm@lundbeck.com>
2024-10-21 10:27:25 +0200

    test

--- lundpy/app.py
+++ lundpy/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Welcome to the Python Best Practices Workshop!"
+    return "Example"
 
 if __name__ == '__main__':
     app.run(debug=True)
