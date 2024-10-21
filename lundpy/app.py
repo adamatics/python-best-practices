07af49c5ecfd718dcf9a5ce23be458d36c3d5452
salvador-g-c <sagm@lundbeck.com>
2024-10-21 10:29:25 +0200

    A random greeting 2.

--- lundpy/app.py
+++ lundpy/app.py
@@ -4,7 +4,7 @@ import random
 app = Flask(__name__)
 
 quotes = [
-    "Code is like humor. When you have to explain it, it’s bad.",
+    "Code is like humor. When you have to explain it, it’s a bad thing.",
     "Fix the cause, not the symptom.",
     "Optimism is an occupational hazard of programming."
 ]
