56fd39ae567af3ff5d09a1e14931ea0e702bf392
cahl-lundbeck <67636831+cahl-lundbeck@users.noreply.github.com>
2024-10-21 10:20:59 +0200

    added random greeting message

--- lundpy/app.py
+++ lundpy/app.py
@@ -1,10 +1,17 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
+quotes = [
+    "Code is like humor. When you have to explain it, it’s bad.",
+    "Fix the cause, not the symptom.",
+    "Optimism is an occupational hazard of programming."
+]
+
 @app.route('/')
 def hello():
-    return "Hello, World from Flask!"
+    return random.choice(quotes)
 
 if __name__ == '__main__':
     app.run(debug=True)
