0e99376d520af384f4d3367079ec84825cff119d
Victor Valera <victor@adamatics.com>
2024-10-24 07:48:11 +0200

    fix: improve base app.py

--- lundpy/app.py
+++ lundpy/app.py
@@ -5,24 +5,24 @@ from fastapi import Request
 app=fastapi.FastAPI()
 
 @app.get('/')
-async def read_root():
- return {"message":"Hello, World"}
+async def read_root():  
+  return {"message" : "Hello, World"}
 
 @app.get('/items/{item_id}')
-async def read_item(item_id):
- return {"item_id":item_id}
+async def read_item( item_id):
+  return {"item_id":item_id}
 
 @app.post('/items/')
 async def create_item(item:dict):
-    return {"item":item}
+  return {"item":item }
 
 @app.get('/users/{user_id}')
-def get_user(user_id:int):
- return {"user_id":user_id}
+def get_user(user_id):
+  return {"user_id":user_id}
 
 @app.post('/users/')
-def create_user(user:dict):
- return {"user":user}
+def create_user(user:dict):   
+  return {"user":user}
 
 if __name__=='__main__':
  uvicorn.run(app,host='127.0.0.1',port=8000)
